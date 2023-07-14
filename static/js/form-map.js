03bd35adbdf4f3e9bd504abab70ebb6e55c4590a
Ivelina-Z <iveta.zhekova@gmail.com>
2023-07-14 09:42:59 +0300

    Animal maps js script refactor.

---
+++ static/js/form-map.js
@@ -0,0 +1,19 @@
+function initMap() {
+    let map = L.map('location_map').setView([42.930, 26.027], 7);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    function onClick(event) {
+        let mapBlock = document.getElementById('location_map');
+        let coordinates = event.latlng;
+        const inputField = document.getElementById('id_location')
+        inputField.value = `POINT (${coordinates.lat} ${coordinates.lng})`;
+        console.log(inputField.value);
+    }
+    map.on('click', onClick);
+}
+window.addEventListener('DOMContentLoaded', (event) => {
+    initMap();
+});
