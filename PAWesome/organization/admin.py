3709d3ae352e40962a5dc57826d9d96aad1eae97
Ivelina-Z <iveta.zhekova@gmail.com>
2023-07-31 23:06:46 +0300

    Organization and Employee admin customization.

--- PAWesome/organization/admin.py
+++ PAWesome/organization/admin.py
@@ -5,9 +5,16 @@ from PAWesome.organization.models import Organization, Employee
 
 @admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
-    pass
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        if not request.user.is_superuser:
+            queryset = queryset.filter(pk=request.user.organization.pk)
+        return queryset
 
 
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ('first_name', 'last_name')
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser
